6666c8e32d1e2cf5319a2d77f24ddedc2582f38d
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 16:25:28 +0530

    feat(problem-021): add user response dto

---
+++ 02-dto-validation/21-entity-dto-mapper/src/main/java/com/practice/entitydtomapper/UserResponse.java
@@ -0,0 +1,33 @@
+package com.practice.entitydtomapper;
+
+public class UserResponse {
+    int id;
+    String name;
+
+    public UserResponse() {
+
+    }
+
+    public UserResponse(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+}
+
