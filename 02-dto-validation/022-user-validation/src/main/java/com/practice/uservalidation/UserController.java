102b5df03d79173cc9920fa7a48e98c74b225099
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 20:25:19 +0530

    feat(problem-022): add user controller

---
+++ 02-dto-validation/022-user-validation/src/main/java/com/practice/uservalidation/UserController.java
@@ -0,0 +1,21 @@
+package com.practice.uservalidation;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public User addUser(@Valid @RequestBody UserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+}
