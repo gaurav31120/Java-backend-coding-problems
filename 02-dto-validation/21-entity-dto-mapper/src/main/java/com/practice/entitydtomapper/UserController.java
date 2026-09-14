4df7654673166939edf8fbd8c3dea273389d126e
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 17:02:56 +0530

    feat(problem-021): add user controller

---
+++ 02-dto-validation/21-entity-dto-mapper/src/main/java/com/practice/entitydtomapper/UserController.java
@@ -0,0 +1,20 @@
+package com.practice.entitydtomapper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/{id}")
+    public UserResponse getUser(@PathVariable int id) {
+        return userService.getUser(id);
+    }
+
+}
