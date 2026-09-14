e87d1187016ac2a1776bf729ddbdf6eb3aeaa462
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 07:06:56 +0530

    feat(problem-017): add user controller

---
+++ 017-response-entity/src/main/java/com/practice/responseentity/UserController.java
@@ -0,0 +1,31 @@
+package com.practice.responseentity;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+
+        User user = userService.findById(id);
+
+        if(user!= null) {
+            return ResponseEntity.ok(user);
+        }
+
+        else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+}
