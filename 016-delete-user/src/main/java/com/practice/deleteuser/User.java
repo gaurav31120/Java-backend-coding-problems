d17db4bfc21732d1b8754f55e7b72081278e44dc
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 06:05:05 +0530

    feat(problem-016): add user model

---
+++ 016-delete-user/src/main/java/com/practice/deleteuser/User.java
@@ -0,0 +1,31 @@
+package com.practice.deleteuser;
+
+public class User {
+    int id;
+    String name;
+
+    public User() {
+
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
