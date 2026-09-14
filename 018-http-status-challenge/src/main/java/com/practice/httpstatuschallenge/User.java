2bb201ede2ed34c5df3812187d6a201640660893
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 07:15:29 +0530

    feat(problem-018): add user model

---
+++ 018-http-status-challenge/src/main/java/com/practice/httpstatuschallenge/User.java
@@ -0,0 +1,32 @@
+package com.practice.httpstatuschallenge;
+
+public class User {
+    int id;
+    String name;
+
+    public User() {
+
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
+
