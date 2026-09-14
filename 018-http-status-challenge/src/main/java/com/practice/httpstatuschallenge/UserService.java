66acb6b45ccf3363522d37e07b6ac310f1194a76
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-14 07:19:16 +0530

    feat(problem-018): add user service

---
+++ 018-http-status-challenge/src/main/java/com/practice/httpstatuschallenge/UserService.java
@@ -0,0 +1,22 @@
+package com.practice.httpstatuschallenge;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository= userRepository;
+    }
+
+    public User findById(int id) {
+        return userRepository.findById(id);
+
+    }
+
+
+
+}
