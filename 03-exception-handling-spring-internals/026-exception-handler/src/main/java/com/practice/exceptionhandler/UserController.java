031feb1b9fd91a8d49d5b10946c84d95e9e02cb6
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-15 09:35:23 +0530

    feat(problem-026): add user controller

--- 03-exception-handling-spring-internals/026-exception-handler/src/main/java/com/practice/exceptionhandler/UserController.java
+++ 03-exception-handling-spring-internals/026-exception-handler/src/main/java/com/practice/exceptionhandler/UserController.java
@@ -1,4 +1,4 @@
-package com.practice.usernotfound;
+package com.practice.exceptionhandler;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
