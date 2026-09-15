9df3a50adccc4e9c3756db0ca6e1e17a65ec5b92
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-15 08:37:51 +0530

    feat(problem-025): add user model

---
+++ 03-exception-handling-spring-internals/025-user-not-found/src/main/java/com/practice/usernotfound/User.java
@@ -0,0 +1,44 @@
+package com.practice.usernotfound;
+
+public class User {
+    int id;
+    String name;
+    String email;
+
+    public User() {
+
+    }
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+}
