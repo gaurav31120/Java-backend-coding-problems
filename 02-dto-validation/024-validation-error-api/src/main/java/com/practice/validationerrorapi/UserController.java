c43a0f44ff75dec645e5231389878ce1e52119a7
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-15 07:39:47 +0530

    feat(problem-024): add registration controller

---
+++ 02-dto-validation/024-validation-error-api/src/main/java/com/practice/validationerrorapi/UserController.java
@@ -0,0 +1,15 @@
+package com.practice.validationerrorapi;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/register")
+    public RegistrationRequest registerUser(@Valid @RequestBody RegistrationRequest registrationRequest) {
+        return registrationRequest;
+    }
+}
