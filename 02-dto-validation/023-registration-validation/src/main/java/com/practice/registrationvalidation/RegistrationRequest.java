7f1b0da48522f08e53c76265a301d030c488565c
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-15 06:44:57 +0530

    feat(problem-023): add registration request validation

---
+++ 02-dto-validation/023-registration-validation/src/main/java/com/practice/registrationvalidation/RegistrationRequest.java
@@ -0,0 +1,52 @@
+package com.practice.registrationvalidation;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegistrationRequest {
+    @NotBlank
+    String name;
+    @NotBlank
+    @Email
+    String email;
+    @NotBlank
+    @Size(min = 8)
+    String password;
+
+    public RegistrationRequest() {
+
+    }
+
+    public RegistrationRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+}
