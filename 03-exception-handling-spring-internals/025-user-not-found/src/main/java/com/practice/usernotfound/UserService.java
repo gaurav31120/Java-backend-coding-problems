0e7c69168139852e80cafb73f9b522f6f6583db9
Gaurav Kumar <gaurav31120@gmail.com>
2026-09-15 08:56:21 +0530

    feat(problem-025): add user service

---
+++ 03-exception-handling-spring-internals/025-user-not-found/src/main/java/com/practice/usernotfound/UserService.java
@@ -0,0 +1,22 @@
+package com.practice.usernotfound;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(int id) {
+        User user = userRepository.findById(id);
+       if(user == null) {
+           throw new UserNotFoundException("User with id " + id + " not found");
+       }
+       return user;
+    }
+
+}
